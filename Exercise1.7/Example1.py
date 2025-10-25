c457eea301f754f7a4c2191e1b32fdb645c8a432
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2025-10-25 16:40:29 -0400

    Add Recipe model __repr__ and create table
    Added a __repr__ method to the Recipe class for improved object representation. Also called Base.metadata.create_all(engine) to ensure the practice_recipes table is created in the database.

--- Exercise1.7/Example1.py
+++ Exercise1.7/Example1.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
-# generates the declarative base class
+# generates the declarative base class // additional properties from SQLAlchemy’s ORM system
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column
 from sqlalchemy.types import Integer, String
 
-engine = create_engine("mysql://cf-python:password@localhost/my_database")
+engine = create_engine(
+    "mysql://cf-python:password@localhost/my_database",
+    # echo=True  # optional: logs SQL so you can see what's happening
+)
 Base = declarative_base()
+Base.metadata.create_all(engine)
 
 class Recipe(Base):
     __tablename__ = "practice_recipes"
@@ -15,3 +19,7 @@ class Recipe(Base):
     ingredients = Column(String(255))
     cooking_time = Column(Integer)
     difficulty = Column(String(20))
+
+    def __repr__(self):
+        return "<Recipe ID: " + str(self.id) + "-" + self.name + ">"
+
